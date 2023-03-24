618cff11824d3c9c8968a616de9ee415c66b21c0
hbcod1 <houcine.bh@pm.me>
2023-03-24 08:38:24 -0700

    set up libraries

--- main.h
+++ main.h
@@ -1,10 +1,9 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
-
-#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdargs.h>
-
+#include <stdarg.h>
+#include <unistd.h>
+int _putchar(char);
 int _printf(const char *format, ...);
 #endif /* end def main.h */
