21bfa9f65efcf66e352ce1c0f6ccf83708bb0a2f
hbcod1 <houcine.bh@pm.me>
2023-03-24 07:40:08 -0700

    adding libraries and prototypes

--- main.h
+++ main.h
@@ -1,4 +1,10 @@
 #ifdef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdargs.h>
+
+int _printf(const char *format, ...);
 #endif /* end def main.h */
