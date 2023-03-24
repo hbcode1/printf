b142e5563f69ae6b7183a7760bdc971f058330f7
MuzikayiseKhuzwayo <khuzwayomuzikayise@gmail.com>
2023-03-24 16:21:09 +0200

    added: branch dev-mk, printf.c; changed: main.h

--- main.h
+++ main.h
@@ -1,4 +1,23 @@
 #ifdef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdargs.h>
+
+/**
+ * struct _printf_case - a structure that holds id and function
+ * @id: identifier character
+ * @_printf_case: function which handles case
+ *
+ * typedef _printf_case_t : type for printcase structure
+ */
+typedef struct _printf_case
+{
+	char id;
+	void (*_printf_case)(va_list *arg);
+} _printf_case_t;
+
+int _printf(const char *format, ...);
+
 #endif /* end def main.h */
---
+++ printf.c
@@ -0,0 +1,15 @@
+/**
+ * _printf - prints out a formatted string
+ * @format: string being printed out
+ * @...: variable input parameters
+ *
+ * Description: Takes in a string @format and variable inputs which change
+ * The string into whatever formatted that is prescribed
+ * Return: integer value describing nuber of chars printed
+ */
+int _printf(const char *format, ...)
+{
+	/**
+	 * DO something here
+	 */
+}
