df04ea06e8b3901e0b445b450d09f6db67ed36a0
MuzikayiseKhuzwayo <khuzwayomuzikayise@gmail.com>
2023-03-27 11:59:22 +0200

    fixed: negative numbers, capitalised hexadecimal

--- printf_case_int.c
+++ printf_case_int.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * capitalise_chars - capitalises characters in a hex for case 17
+ * @str: given string that will be resolved and capitallised
+ *
+ * Return: pointer to string
+ */
+void capitalise_chars(char *str)
+{
+	int i = 0;
+	for (i = 0; i < 8; i++)
+	{
+		if (str[i] > 96 && str[i] < 123)
+		{
+			str[i] = str[i] - 'a' + 'A';
+		}
+	}
+}
+/**
  * print_case_int - prints case for format of d
  * @arg: list of arguments
  * @specifier: int number that specifies case for output
@@ -26,6 +43,11 @@ int print_case_int(va_list *arg, int specifier)
 	if (specifier == 0)
 	{
 		specifier = 10, num = va_arg(*arg, int);
+		if (num < 0)
+		{
+			num *= -1;
+			len += _putchar('-');
+		}
 		temp = itoa(num, temp, specifier);
 	}
 	if (specifier == 2)
@@ -40,10 +62,11 @@ int print_case_int(va_list *arg, int specifier)
 	}
 	if (specifier == 8 || specifier == 16 || specifier == 17)
 	{
-		if (specifier == 17)
-			specifier--;
+
 		num2 = va_arg(*arg, int);
 		temp = itoa(num2, temp, specifier);
+		if (specifier == 17)
+			capitalise_chars(temp);
 	}
 	while (*temp)
 		len += _putchar(*temp++);
