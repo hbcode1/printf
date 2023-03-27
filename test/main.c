995247c21c81806ac3484c31d18c648f12645478
MuzikayiseKhuzwayo <khuzwayomuzikayise@gmail.com>
2023-03-27 18:22:02 +0200

    fixed: 0(NULL case for strings)

--- test/main.c
+++ test/main.c
@@ -9,12 +9,21 @@
  */
 int main(void)
 {
-	_printf("Let's try to printf a simple sentence.\n");
-	printf("Let's try to printf a simple sentence.\n");
-	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
-	printf("String:[%s]\n", "I am a string !");
+	char *temp = NULL;
+	int len1;
+	int len2;
+	len1 = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	printf("len1 is %d, len2 is %d\n", len1, len2);
+	len1 = _printf("Character:[%c]\n", 'H');
+	len2 = printf("Character:[%c]\n", 'H');
+	printf("len1 is %d, len2 is %d\n", len1, len2);
+	len1 = _printf("String:[%s]\n", "I am a string !");
+	len2 = printf("String:[%s]\n", "I am a string !");
+	printf("len1 is %d, len2 is %d\n", len1, len2);
+	len1 = _printf("%s\n", temp);
+	len2 = printf("%s", temp);
+	printf("len1 is %d, len2 is %d\n", len1, len2);
 
 	return (0);
 }
